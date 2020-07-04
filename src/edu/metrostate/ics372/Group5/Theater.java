cbd35d18ef64396d5c0394f2d74ded344eb629da
Anthony Nguyen <nguynaanthony@gmail.com>
2020-07-04 16:45:29 -0500

    creditcard/phone validation patch

--- src/edu/metrostate/ics372/Group5/Theater.java
+++ src/edu/metrostate/ics372/Group5/Theater.java
@@ -42,6 +42,124 @@ public class Theater implements Serializable {
     }
 
     /**
+     * This method will check if credit card creditCardNumber is valid, this only
+     * accept American Express, MasterCard, Visa, and Discover card
+     * 
+     * @param creditCardNumber credit card to check
+     * @return true if card is valid
+     */
+    public static boolean isValidCard(long creditCardNumber) {
+	return (getSize(creditCardNumber) >= 13 && getSize(creditCardNumber) <= 16)
+		&& (prefixMatched(creditCardNumber, 4) || prefixMatched(creditCardNumber, 5)
+			|| prefixMatched(creditCardNumber, 37) || prefixMatched(creditCardNumber, 6))
+		&& ((sumOfDoubleEvenPlace(creditCardNumber) + sumOfOddPlace(creditCardNumber)) % 10 == 0);
+    }
+
+    // return sum of credit card numbers
+    public static int sumOfDoubleEvenPlace(long creditCardNumber) {
+	int sum = 0;
+	String num = creditCardNumber + "";
+	for (int i = getSize(creditCardNumber) - 2; i >= 0; i -= 2)
+	    sum += getDigit(Integer.parseInt(num.charAt(i) + "") * 2);
+
+	return sum;
+    }
+
+    // Return this creditCardNumber if it is a single digit, otherwise,
+    // return the sum of the two digits
+    public static int getDigit(int creditCardNumber) {
+	if (creditCardNumber < 9)
+	    return creditCardNumber;
+	return creditCardNumber / 10 + creditCardNumber % 10;
+    }
+
+    // Return sum of odd-place digits in creditCardNumber
+    public static int sumOfOddPlace(long creditCardNumber) {
+	int sum = 0;
+	String num = creditCardNumber + "";
+	for (int i = getSize(creditCardNumber) - 1; i >= 0; i -= 2)
+	    sum += Integer.parseInt(num.charAt(i) + "");
+	return sum;
+    }
+
+    // Return true if the digit d is a prefix for creditCardNumber
+    public static boolean prefixMatched(long creditCardNumber, int d) {
+	return getPrefix(creditCardNumber, getSize(d)) == d;
+    }
+
+    // Return the creditCardNumber of digits in d
+    public static int getSize(long d) {
+	String num = d + "";
+	return num.length();
+    }
+
+    // Return the first k creditCardNumber of digits from
+    // creditCardNumber. If the creditCardNumber of digits in creditCardNumber
+    // is less than k, return creditCardNumber.
+    public static long getPrefix(long creditCardNumber, int k) {
+	if (getSize(creditCardNumber) > k) {
+	    String num = creditCardNumber + "";
+	    return Long.parseLong(num.substring(0, k));
+	}
+	return creditCardNumber;
+    }
+
+    /**
+     * Check if credit card expiration date is valid
+     * 
+     * @param date
+     * @return
+     */
+    public static boolean isDate(String date) {
+	if ((date.length() == 5) && Character.isDigit(date.charAt(0)) && Character.isDigit(date.charAt(1))
+		&& Character.isDigit(date.charAt(3)) && Character.isDigit(date.charAt(4))
+		&& containsSpecialCharacter(date) && isMonthYearValid(date)) {
+	    return true;
+	}
+	return false;
+    }
+
+    /**
+     * Checks if month and year is a valid
+     * 
+     * @param date the month and year
+     * @return true if valid
+     */
+    public static boolean isMonthYearValid(String date) {
+	String[] parts = date.split("/");
+	int month = Integer.parseInt(parts[0]);
+	int year = Integer.parseInt(parts[1]);
+	if (month <= 12 && month >= 1 && year <= 35 && year >= 20) {
+	    return true;
+	}
+	return false;
+    }
+
+    /**
+     * Checks if string contains /
+     * 
+     * @param date checks if have /
+     * @return true if have /
+     */
+    public static boolean containsSpecialCharacter(String date) {
+	if (date.substring(2, 3).matches("/")) {
+	    return true;
+	}
+	return false;
+    }
+
+    /**
+     * Checks if phone is a valid number
+     * 
+     * @param phone check phone if valid
+     * @return true if valid
+     */
+    public static boolean validPhone(String phone) {
+	String regex = "\\d{3}-\\d{3}-\\d{4}"; // XXX-XXX-XXXX
+	return phone.matches(regex);
+    }
+
+    /**
      * This method will add a customer to the CustomerList collection and checks if
      * the customer exists in the system
      * 
@@ -220,10 +338,10 @@ public class Theater implements Serializable {
 	return insert;
     }
 
-	public static Client searchClient(String clientID) {
-		Client client;
-		client = ClientList.search(clientID);
-		return client;
-	}
+    public static Client searchClient(String clientID) {
+	Client client;
+	client = ClientList.search(clientID);
+	return client;
+    }
 
 }
